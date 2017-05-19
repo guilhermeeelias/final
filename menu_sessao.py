2b6e3891064d3b7f0d2eb82549132cee6d74cb46
danielmoretti <daniel@moretti.com.br>
2017-05-19 02:00:54 -0300

    Update menu_sessao.py

--- menu_sessao.py
+++ menu_sessao.py
@@ -11,15 +11,15 @@ def menu_criar():
     cod_sessao=int(input("Codigo do ator: "))
     cod_filme=int(input("Codigo do filme: "))
     cod_sala=int(input("Codigo da sala: "))
-    horario=int(input("Horario: "))
-    sessao.criar_seesao(cod_sessao, sessao_filme, cod_sala, horario)
+    horario=str(input("Horario: "))
+    sessao.criar_sessao(cod_sessao, sessao_filme, cod_sala, horario)
 
 
 def menu_listar():
     print("\nListar Sessoes: ")
-        sessoes=sessao.listar_sessoes()
-        for j in sessoes
-            imprimir_sessao(j)
+    sessoes=sessao.listar_sessoes()
+    for j in sessoes:
+        imprimir_sessao(j)
 
 def menu_buscar():
     print("\nBuscar sessao por codigo: \n")
@@ -32,14 +32,14 @@ def menu_buscar():
 
 def menu_remover():
     print ("\nRemover sessao\n ")
-    cod_sessao=int(int("Codigo da sessão: "))
+    cod_sessao=int(input("Codigo da sessão: "))
     j=sessao.remover_sessoes(cod_sessao)
     if (j==False):
         print("Sessão não encontrada ")
     else:
             print("Sessão removida")
 
-def monstrar_menu():
+def mostrar_menu():
     run_sessao=True
     menu=("\n------------------------\n"+
                     "(1)Criar sessão \n"+
@@ -50,17 +50,18 @@ def monstrar_menu():
                     "(0) Voltar\n"+
                     "-------------------")
 
-while(run_sessao):
-    print(menu)
-    op=int(int("Digite sua escolha: "))
-    if(op==1):
-        menu_criar()
-    elif (op==2):
-        menu_listar()
-    elif(op==3)
-        menu_buscar()
-    elif(op==4)
-        menu_remover()
+    while(run_sessao):
+        print(menu)
+        op=int(input("Digite sua escolha: "))
+        if(op==1):
+            menu_criar()
+        elif(op==2):
+            menu_listar()
+        elif(op==3):
+            menu_buscar()
+        elif(op==4):
+            menu_remover()
 
-    elif (op==0):
-        run_sessao=False
+        elif (op==0):
+            run_sessao=False
+
